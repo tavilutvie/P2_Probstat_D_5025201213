74e67ed23a76519c8d3e00d3504c677cc780d5d1
Tavi <85897222+tavilutvie@users.noreply.github.com>
2022-05-29 23:22:25 +0700

    Add files via upload

--- D_5025201213.r
+++ D_5025201213.r
@@ -1,5 +1,5 @@
-#Soal1
-x <- c(78, 75, 67, 77, 70, 72, 28, 74, 77)
+#Soal 1
+x <- c(78, 75, 67, 77, 70, 72, 78, 74, 77)
 y <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
 
 #a
@@ -18,8 +18,7 @@ t.test(x, y, mu = 0, alternative = "two.sided", var.equal = TRUE, conf.level = 0
 
 #P-value lebih kecil dibandingkan tingkat signifikansi (0,05) maka hipotesis nol tidak berlaku.
 
-#Soal2
-install.packages("BSDA")
+#Soal 2
 library(BSDA)
 
 #a
@@ -37,37 +36,31 @@ tsum.test(mean.x=23500, sd(3900), n.x=100, var.equal = TRUE, alternative = "grea
 
 
 
-#Soal3
+#Soal 3
 
 #a
-#H0:
-z1=(3.64-0)/(1.67/sqrt(19))
-#H1:
-z2=(2.79-0)/(1.32/sqrt(27))
+#H0: rata-rata saham bandung == saham bali
+#H1: rata-rata saham bandung != saham bali
 
 #b
 
 
-#Soal4
+
+#Soal 4
+kucingITS <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"),h=T)
+attach(kucingITS)
+names(kucingITS)
+
 #a
-dataoneway <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"),h=T)
-attach(dataoneway)
-names(dataoneway)
+kucingITS$Group <- as.factor(kucingITS$Group)
+kucingITS$Group = factor(kucingITS$Group,labels = c("Kucing oren", "Kucing hitam", "Kucing putih"))
 
-dataoneway$Group <- as.factor(dataoneway$Group)
-dataoneway$Group = factor(dataoneway$Group,labels = c("Kucing oren", "Kucing hitam", "Kucing putih"))
+class(kucingITS$Group)
 
-class(dataoneway$Group)
+Group1 <- subset(kucingITS, Group == "Kucing oren")
+Group2 <- subset(kucingITS, Group == "Kucing hitam")
+Group3 <- subset(kucingITS, Group == "Kucing putih")
 
-Group1 <- subset(dataoneway, Group == "Kucing oren")
-Group2 <- subset(dataoneway, Group == "Kucing hitam")
-Group3 <- subset(dataoneway, Group == "Kucing putih")
-
-#b
-bartlett.test(Length ~ Group, data = dataoneway)
-#Kesimpulannya: Bartlett's K-squared = 0.43292, df = 2, dan p-value = 0.08054
-
-#c
 qqnorm(Group1$Length)
 qqline(Group1$Length)
 
@@ -77,20 +70,57 @@ qqline(Group2$Length)
 qqnorm(Group2$Length)
 qqline(Group2$Length)
 
+#b
+bartlett.test(Length ~ Group, data = kucingITS)
+#p-value = 0.08054
+#Kesimpulannya: Bartlett's K-squared = 0.43292 dan df = 2
+
+#c
+model1 = lm(Length ~ Group, data = kucingITS)
+anova(model1)
+
 #d
-pvalue = 0.8054
-#H0 ditolak
+#pvalue = 0.0013 < tingkat signifikansi(0.5). H0 ditolak.
 
 #e
-model1 = lm(Length ~ Group, data = dataoneway)
-anova(model1)
 TukeyHSD(aov(model1))
 
 #f
-install.packages("ggplot2")
 library("ggplot2")
 
-ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + scale_x_discrete() + xlab("Treatment Group") + ylab("Length (cm)")
+ggplot(kucingITS, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + scale_x_discrete() + xlab("Treatment Group") + ylab("Length (cm)")
 
-#Soal5
+#Soal 5
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+GLASSTEMPLIGHT <- read.csv("https://drive.google.com/u/0/uc?id=1aLUOdw_LVJq6VQrQEkuQhZ8FW43FemTJ&export=download")
+head(GLASSTEMPLIGHT)
+str(GLASSTEMPLIGHT)
+
+#a
+qplot(x = Temp, y = Light, geom = "point", data = GLASSTEMPLIGHT) + facet_grid(.~Glass, labeller = label_both)
+
+#b
+GLASSTEMPLIGHT$Glass <- as.factor(GLASSTEMPLIGHT$Glass)
+GLASSTEMPLIGHT$Temp_Factor <- as.factor(GLASSTEMPLIGHT$Temp)
+str(GLASSTEMPLIGHT)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GLASSTEMPLIGHT)
+summary(anova)
+
+#c
+data_summary <- group_by(GLASSTEMPLIGHT, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+#d
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+#e
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
 
